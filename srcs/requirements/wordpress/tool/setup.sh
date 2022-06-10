e03ba349857b982132ebe0f2f25134e6586cd276
LeVraiDwi <67096994+LeVraiDwi@users.noreply.github.com>
2022-06-10 15:22:15 +0200

    Update setup.sh

--- srcs/requirements/wordpress/tool/setup.sh
+++ srcs/requirements/wordpress/tool/setup.sh
@@ -6,7 +6,7 @@ if [ ! -f /var/www/wordress/wp-config.php ]; then
 	while [ ! -f /var/www/wordpress/wp-config.php ]; do
 		wp core config --allow-root --dbname="wordpress" --dbuser="tcosse" --dbpass="tcosse" --dbhost='mariadb:3306'
 	done
-	wp core install --allow-root --url='tcosse.42.fr' --title='wordpress inception' --admin_user='tcosse' --admin_password='tcosse' --admin-email='tcosse@student.42.fr' --path='/var/www/wordpress'
+	wp core install --allow-root --url='tcosse.42.fr' --title='wordpress inception' --admin_user='tcosse' --admin_password='tcosse' --admin_email='tcosse@student.42.fr' --path='/var/www/wordpress'
 
 fi
 
